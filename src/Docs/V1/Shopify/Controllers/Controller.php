62148b00297fc8e326cfcfe843c55b26cf5af8f8
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-09 11:34:39 +0800

    add shopify api docs

---
+++ src/Docs/V1/Shopify/Controllers/Controller.php
@@ -0,0 +1,25 @@
+<?php
+namespace Apps\Shopify\Docs\V1\Shopify\Controllers;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="NexaMerchant Shopify Rest API Documentation",
+ *      description="NexaMerchant Shopify Rest API Documentation",
+ *
+ *      @OA\Contact(
+ *          email="nice.lizhi@gmail.com"
+ *    )
+ * )
+ * @OA\Server(
+ *    url="http://localhost:8000/api/v1",
+ *   description="NexaMerchant Shopify End Rest API"
+ * )
+ * @OA\Tag(
+ *  name="Shopify",
+ * description="API Endpoints of Shopify"
+ * )
+ */
+class Controller
+{
+}
