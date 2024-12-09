bfbcc7f1198e240fcd5c210b9cf3b7d0e11131ca
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-09 12:10:20 +0800

    add shopify admin

---
+++ src/Http/Controllers/Api/V1/Admin/ProductsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NexaMerchant\Shopify\Http\Controllers\Api\V1\Admin;
+
+use NexaMerchant\Shopify\Http\Controllers\Api\Controller;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    /**
+     * 
+     * sync products from shopify to nexamerchant
+     * 
+     * @param int $id
+     * 
+     * @return \Illuminate\Http\Response
+     * 
+     * @throws \Exception
+     * 
+     */
+    public function sync($id) {
+        
+    }
+}
--- src/Http/Controllers/Api/V1/OrdersController.php
+++ src/Http/Controllers/Api/V1/OrdersController.php
@@ -7,6 +7,6 @@ class OrdersController extends Controller
 {
     public function index()
     {
-        return response()->json(['message' => 'OrdersController@index']);
+        
     }
 }
