3515378e21813a14762e4603bcf963824bb1248a
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-09 13:55:42 +0800

    add admin api url

--- src/Routes/admin.php
+++ src/Routes/admin.php
@@ -9,80 +9,43 @@ use NexaMerchant\Shopify\Http\Controllers\Admin\ReportsController;
 use NexaMerchant\Shopify\Http\Controllers\Admin\SettingsController;
 use NexaMerchant\Shopify\Http\Controllers\Admin\OrdersController;
 use NexaMerchant\Shopify\Http\Controllers\Admin\DiscountsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\GiftCardsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\TransactionsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\RefundsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\CheckoutsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\DraftOrdersController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\AbandonedCheckoutsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\ShippingController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\TaxController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\BillingController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\FilesController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\GiftCardProductsController;
-// use NexaMerchant\Shopify\Http\Controllers\Admin\GiftCardBatchesController;
 
-Route::group(['middleware' => ['admin','admin_option_log'], 'prefix' => config('app.admin_url')], function () {
+Route::group([
+    'prefix'     => 'api/v1/admin',
+    'middleware' => ['sanctum.locale','assign_request_id'],
+], function () {
 
-    Route::prefix('shopify')->group(function () {
+    Route::controller(ProductsController::class)->prefix('shopify')->group(function () {
+        Route::get('', 'allResources');
 
+        Route::post('', 'store');
 
-        // Example
-        Route::controller(ExampleController::class)->prefix('example')->group(function () {
+        Route::get('{id}', 'getResource');
 
-            Route::get('demo', 'demo')->name('Shopify.admin.example.demo');
+        Route::put('{id}', 'update');
 
-        });
+        Route::delete('{id}', 'destroy');
 
+        Route::post('mass-update', 'massUpdate');
 
-        // Products
-        Route::controller(ProductsController::class)->prefix('products')->group(function () {
+        Route::post('mass-destroy', 'massDestroy');
 
-            Route::get('index', 'index')->name('Shopify.admin.products.index');
+        Route::post('quick-create', 'quickCreate');
 
-        });
+        Route::get('{id}/sync', 'sync'); // sync shopify data
 
-        // Customers
-        Route::controller(CustomersController::class)->prefix('customers')->group(function () {
+        Route::get('{id}/clean-cache', 'cleanCache'); // clean cache
 
-            Route::get('index', 'index')->name('Shopify.admin.customers.index');
+        Route::get('{id}/ad-images', 'adImages'); // ad images
 
-        });
+        Route::post('{id}/ad-images', 'saveAdImage'); // save ad image
 
-        // Stores
-        Route::controller(StoresController::class)->prefix('stores')->group(function () {
+        Route::get('{id}/sell-points', 'sellPoints'); // sell points
 
-            Route::get('index', 'index')->name('Shopify.admin.stores.index');
+        Route::post('{id}/sell-points', 'saveSellPoint'); // save sell point
 
-        });
-
-        // Reports
-        Route::controller(ReportsController::class)->prefix('reports')->group(function () {
-
-            Route::get('index', 'index')->name('Shopify.admin.reports.index');
-
-        });
-
-        // Settings
-        Route::controller(SettingsController::class)->prefix('settings')->group(function () {
-
-            Route::get('index', 'index')->name('Shopify.admin.settings.index');
-
-        });
-
-        // Orders
-        Route::controller(OrdersController::class)->prefix('orders')->group(function () {
-
-            Route::get('index', 'index')->name('Shopify.admin.orders.index');
-
-        });
-
-        // Discount
-        Route::controller(DiscountsController::class)->prefix('discounts')->group(function () {
-
-            Route::get('index', 'index')->name('Shopify.admin.discounts.index');
-
-        });
 
     });
+    
+
 });
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -1,18 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use NexaMerchant\Shopify\Http\Controllers\Api\ExampleController;
 
-Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
-     Route::prefix('shopify')->group(function () {
-
-        Route::controller(ExampleController::class)->prefix('example')->group(function () {
-
-            Route::get('demo', 'demo')->name('shopify.api.example.demo');
-
-        });
-
-     });
-});
-
-include __DIR__ . '/api.v1.php';
+include __DIR__ . '/api.v1.php'; // api v1 routes
+include __DIR__ . '/admin.php'; // admin api routes
